937345d64cb9a25816458fd0dde2c81460da1c0e
zbysulak <sedlik31@gmail.com>
2022-08-04 17:07:51 +0200

    Guess animation

--- src/components/GuessItem.vue
+++ src/components/GuessItem.vue
@@ -1,25 +1,27 @@
 <template>
   <v-row @click="$emit('guessClicked', guessColors)">
-    <div class="guess my-2">
+    <div class="guess my-4" translate="yes">
       <v-sheet
         v-for="c in guessColors"
         v-bind:key="c"
-        class="mx-2 rounded-pill guess-item"
+        class="mx-2 rounded-pill guess-item animate"
         :style="{ backgroundColor: c }"
       ></v-sheet>
     </div>
     <div class="d-flex justify-center align-center">
       <div
-        class="result color rounded-circle"
+        class="result color rounded-circle animate"
         v-for="c in guess.color"
         v-bind:key="c"
       ></div>
       <div
-        class="result position rounded-circle"
+        class="result position rounded-circle animate"
         v-for="c in guess.position"
         v-bind:key="c"
       ></div>
-      <v-icon v-if="guess.color == 0 && guess.position == 0">mdi-cancel</v-icon>
+      <v-icon class="animate" v-if="guess.color == 0 && guess.position == 0"
+        >mdi-cancel</v-icon
+      >
     </div>
   </v-row>
 </template>
@@ -31,11 +33,51 @@ const guessColors = Object.values(props.guess.colors)
 </script>
 
 <style scoped>
+@keyframes splash {
+  from {
+    opacity: 0;
+    transform: scale(0);
+  }
+  50% {
+    opacity: 1;
+    transform: scale(1.1);
+  }
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+
 .guess {
   height: 3em;
 }
 
+.animate:nth-child(1) {
+  animation-delay: 0;
+}
+.animate:nth-child(2) {
+  animation-delay: 100ms;
+}
+.animate:nth-child(3) {
+  animation-delay: 200ms;
+}
+.animate:nth-child(4) {
+  animation-delay: 300ms;
+}
+.animate:nth-child(5) {
+  animation-delay: 400ms;
+}
+.animate:nth-child(6) {
+  animation-delay: 500ms;
+}
+
+.animate {
+  transform: scale(0);
+  animation: splash 400ms normal forwards ease-in-out;
+}
+
 .guess-item {
+  animation-iteration-count: 1;
   display: inline-block;
   width: 6.4em;
   height: 3em;
