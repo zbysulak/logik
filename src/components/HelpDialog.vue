6a3974ef81861101cfe5192dd72a7c357dcf4c6b
zbysek.sedlacek <zbysek.sedlacek@digiteqautomotive.com>
2022-10-05 13:00:53 +0200

    Help styling

--- src/components/HelpDialog.vue
+++ src/components/HelpDialog.vue
@@ -1,16 +1,16 @@
 <template>
-  <v-dialog v-model="dialog" :fullscreen="$vuetify.display.mobile">
+  <v-dialog v-model="dialog" :fullscreen="$vuetify.display.mobile" :class="$vuetify.display.mobile?'ma-5':''">
     <template v-slot:activator="{ props }">
       <mobile-icon-button v-bind="props" class="ml-2" mobile-icon="mdi-help">Help</mobile-icon-button>
     </template>
-    <v-card color="basil">
+    <v-card color="basil" :class="$vuetify.display.mobile?'rounded':''">
       <v-card-title class="text-center justify-center py-6"> Help</v-card-title>
-      <v-tabs v-model="tab" background-color="transparent" grow>
+      <v-tabs v-model="tab" background-color="transparent">
         <v-tab value="rules"> Rules</v-tab>
         <v-tab value="controls"> Controls</v-tab>
       </v-tabs>
 
-      <v-window v-model="tab">
+      <v-window v-model="tab" class="flex-grow-1">
         <v-window-item value="rules">
           <v-card color="basil" flat>
             <v-card-text>
@@ -22,7 +22,7 @@
                 After each guess you will get result of combination of black and
                 white dots.
               </p>
-              <ul>
+              <ul class="ml-2">
                 <li>White dot means one of your colors is correct.</li>
                 <li>
                   Black dot means one of your colors is correct AND also its
@@ -37,7 +37,7 @@
           <v-card color="basil" flat>
             <v-card-text>
               <p>To start a game you have to select few options first.</p>
-              <ul>
+              <ul class="ml-2">
                 <li>
                   'Number of slots' is number of colors you have to guess.
                 </li>
@@ -50,13 +50,11 @@
                   in two slots, which also increases difficulty.
                 </li>
               </ul>
-              <p>
-                After game starts you can start guessing by selecting your
+              <p>After game starts you can start guessing by selecting your
                 colors and pressing 'Check' button.
               </p>
               <p>'Clear' buttons resets you selection.</p>
-              <p>
-                You can 'go back' to your previous selection by clicking on the
+              <p>You can 'go back' to your previous selection by clicking on the
                 guess.
               </p>
               <p>It is possible to swap colors by drag&droping them.</p>
@@ -81,4 +79,16 @@ const dialog = ref(false)
 const tab = ref(null)
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+p {
+  margin-bottom: 1em;
+}
+
+li {
+  margin-bottom: .7em;
+}
+
+ul {
+  list-style-type: none;
+}
+</style>
